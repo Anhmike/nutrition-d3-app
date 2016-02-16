03e89627ed8406093e4673bb3f5fe9aaef57ec4e
Shijir Tsogoo <shijir99@gmail.com>
2016-02-16 01:21:58 -0800

    initialized the project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nutriAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nutriAppApp
+ */
+angular.module('nutriAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nutriAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nutriAppApp
+ */
+angular.module('nutriAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
